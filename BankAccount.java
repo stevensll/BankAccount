b520a977f46e173f0d2fcd8d48539883e5adbfad
Steven <deltaorb637@gmail.com>
2020-10-27 20:20:24 -0400

    Created and tested getBalance(), getAccountID, and setPassword() methods.

--- BankAccount.java
+++ BankAccount.java
@@ -1,3 +1,20 @@
 public class BankAccount {
-    
+    private double balance;
+    private int accountID;
+    private String password;
+
+    public BankAccount(int ID, String pass){
+        balance = 0;
+        accountID = ID;
+        password = pass;
+    }
+    public double getBalance() {
+        return balance;
+    }
+    public int getAccountID() {
+        return accountID;
+    }
+    public void setPassword(String newPass){
+        password = newPass;
+    }
 }
--- Tester.java
+++ Tester.java
@@ -1,6 +1,8 @@
 public class Tester {
     public static void main (String[] args) {
-        
+        BankAccount testAcct = new BankAccount(95, "somepassword");
+        System.out.println(testAcct.getBalance());
+        System.out.println(testAcct.getAccountID());
+        testAcct.setPassword("somenewpassword");
     }
-
 }
